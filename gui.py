2063cdca63df7a1a509f39f6a65d697f7f7fdcdb
José <josemfb@gmail.com>
2024-02-23 13:43:44 -0300

    Program to format Quintanet files

--- gui.py
+++ gui.py
@@ -1,10 +1,91 @@
-def get_file(message: str) -> str:
-    # return input(message)
-    return ""
+import math
+import os
+import sys
+import tkinter as tk
+from tkinter import filedialog as fd
 
 
-def menu(options: list):
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def get_file() -> str:
+    filetypes = (('Excel files', '*.xls'), ('HTML files', '*.html'), ('All files', '*.*'))
+    file = fd.askopenfilename(title='Open a file', initialdir='/', filetypes=filetypes)
+    return file
+
+
+def show_message(title: str = "", message: str = "", button_text: str = "Continuar") -> None:
+    # Create the root window
+    root = tk.Tk()
+    root.title(title)
+    root.configure(bg="white")
+    root.resizable(False, False)
+
+    logo = tk.PhotoImage(file=resource_path("logo.png"))
+    image = tk.Label(root, bg="white", image=logo)
+    text = tk.Message(root, bg="white", text=f"{message}", justify="left", font=("Vollkorn", 12),
+                      fg="#004000", width=500)
+    title = tk.Label(root, bg="white", text=f"Quinta Compañía «Bomba Arturo Prat»", justify="left",
+                     font=("Vollkorn SC", 12, "bold"), fg="#004000")
+    button = tk.Button(root, text=button_text, command=root.destroy, font=("Vollkorn", 10))
+
+    image.grid(row=0, column=0, rowspan=4, ipadx=10, sticky="N")
+    title.grid(row=1, column=1, ipady=10, sticky="S")
+    text.grid(row=2, column=1, ipadx=10, sticky="NW")
+    button.grid(row=3, column=1, pady=10)
+    blank = tk.Message(root, bg="white")
+    blank.grid(row=0, column=1)
+
+    root.mainloop()
+
+
+def show_options(options: list, title: str = "", message: str = "") -> str:
+    # Create the root window
+    root = tk.Tk()
+    root.title(title)
+    root.configure(bg="white")
+    root.resizable(False, False)
+
+    logo = tk.PhotoImage(file=resource_path("logo.png"))
+    image = tk.Label(root, bg="white", image=logo)
+    text = tk.Message(root, bg="white", text=f"{message}", justify="left", font=("Vollkorn", 12),
+                      fg="#004000", width=500)
+    title = tk.Label(root, bg="white", text=f"Quinta Compañía «Bomba Arturo Prat»", justify="left",
+                     font=("Vollkorn SC", 12, "bold"), fg="#004000")
+
+    image.grid(row=0, column=0, rowspan=4, ipadx=10, sticky="N")
+    title.grid(row=1, column=1, ipady=10, sticky="S")
+    text.grid(row=2, column=1, ipadx=10, sticky="NW")
+
+    buttons = []
+    frame = tk.Frame(root)
+    frame.configure(bg="white")
+    frame.grid(row=3, column=1, padx=10, pady=10)
+
+    blank = tk.Message(root, bg="white")
+    blank.grid(row=0, column=1)
+
+    def button_click(txt: str):
+        global to_return
+        to_return = txt
+        root.destroy()
+
     for i in range(len(options)):
-        print(f"{i + 1}. {options[i]}")
-    selection = int(input("> "))
-    return selection
+        buttons.append(tk.Button(frame, text=options[i], width=8, font=("Vollkorn", 10)))
+    for n, button in enumerate(buttons):
+        pack_to = n // 5
+        button.configure(command=lambda txt=button.cget("text"): button_click(txt))
+        button.grid(row=pack_to, column=n % 5, padx=5, pady=5, sticky="N")
+
+    root.mainloop()
+    return to_return
+
+
+if __name__ == "__main__":
+    print("Only for use as a module")
--- main.py
+++ main.py
@@ -1,22 +1,52 @@
+# pyinstaller -F -w -n "ayudantia" --add-data "logo.png:." --exclude-module "config.py" main.py
+
 import datetime as dt
-import os
 import locale
+import os
+import sys
 
 import openpyxl
 import openpyxl.styles
 import pandas as pd
 
+import config
 import gui
 
-locale.setlocale(locale.LC_ALL, 'es_CL.utf8')
+
+def main() -> int:
+    # options = ["Reportes mensuales (cuadro y parte)",
+    #            "Reportes trimestrales (obligatorios y porcentajes)",
+    #            "Configuración"]
+    # while True:
+    #     match gui.menu(options):
+    #         case 1:
+    #             return generate_monthly()
+    #         case 2:
+    #             return generate_quarterly()
+    #         case 3:
+    #             settings()
+    #         case _:
+    #             return 1
+    return generate_monthly()
 
 
-def generate_monthly():
+def generate_monthly() -> int:
+    # Welcome
+    gui.show_message(title="Reportes mensuales",
+                     message="Se generarán de manera automática el cuadro mensual y el"
+                             "\nparte de asistencia, a partir del archivo que se puede descargar"
+                             "\ndesde Quintanet."
+                             "\nA continuación deberá cargar el archivo .xls obtenido en"
+                             "\nQuintanet. Es importante haber seleccionado las fechas"
+                             "\ncorrespondientes a un solo mes, y haber desmarcado la opción"
+                             "\n“Solo obligatorios”.",
+                     button_text="Cargar archivo")
+
     # Ask user for Excel file path
     # Defaults to excel.html
-    file = gui.get_file("Indica la ubicación del excel generado por Quintanet > ")
+    file = gui.get_file()
     if file == "":
-        file = "excel.html"
+        return 1
 
     # If file extension is .xls change it to .html
     if file[-4:] == ".xls":
@@ -40,6 +70,83 @@ def generate_monthly():
 
     # Add lines for date, hours and type
     col_names = df.columns.tolist()
+
+    # Review acts
+    are_rog = False
+    for act in col_names:
+        day = f"{int(act[0:2]):02d}/{int(act[3:5]):02d}"
+        time = f"{int(act[12:14]):02d}:{int(act[15:17]):02d}"
+
+        # Detect ROG, change it to REG
+        if act[19:] == "ROG":
+            gui.show_message(title="Alerta: ROG",
+                             message=f"Se detectó una ROG el {day} a las {time}."
+                                     f"\nEl reglamento de Compañía señala que todas las reuniones"
+                                     f"\ncitadas por el Directorio serán extraordinarias, por lo"
+                                     f"\nque se cambió automáticamente de “Reunión Ordinaria"
+                                     f"\nGeneral” a “Reunión a Extraordinaria”."
+                             )
+            # TODO - Update DF
+            ...
+            are_rog = True
+
+        # Detect 10-1, ask for correct classification
+        elif act[19:] == "10-1":
+            options = ["10-1-1", "10-1-2", "10-1-3", "10-1-4",
+                       "10-1-5", "10-1-6", "10-1-7", "10-1-8"]
+            correct_act = gui.show_options(options, title="Alerta: 10-1",
+                                           message=f"Se detectó un 10-1 el {day} a las {time}.\n"
+                                                   f"Indique a que acto corresponde en realidad")
+            # TODO - Update DF
+            ...
+
+        # Detect 10-6, ask for correct classification
+        elif act[19:] == "10-6":
+            options = ["10-6-1", "10-6-2", "10-6-3"]
+            correct_act = gui.show_options(options, title="Alerta: 10-6",
+                                           message=f"Se detectó un 10-6 el {day} a las {time}.\n"
+                                                   f"Indique a que acto corresponde en realidad")
+            # TODO - Update DF
+            ...
+
+        # Detect FDoM, ask if it's a FDoM or a FQ
+        elif act[19:] == "FDoM":
+            options = ["FDoM", "FQ"]
+            correct_act = gui.show_options(options, title="Alerta: Funeral",
+                                           message=f"Se detectó un funeral el {day} a las {time}.\n"
+                                                   f"Indique a que acto corresponde en realidad")
+            # TODO - Update DF
+            ...
+
+        # Detect 10-17-3, ask if it's a 10-0-6, 10-17 or 10-18
+        elif act[19:] == "10-7-3":
+            options = ["10-0-6", "10-17", "10-18"]
+            correct_act = gui.show_options(options, title="Alerta: 10-7-3",
+                                           message=f"Se detectó un 10-7-3 el {day} a las {time}.\n"
+                                                   f"Indique a que acto corresponde en realidad")
+            if correct_act == "10-17":
+                options = ["10-17-0", "10-17-1", "10-17-2", "10-17-3", "10-17-4",
+                           "10-17-5", "10-17-6", "10-17-7", "10-17-8"]
+                correct_act = gui.show_options(options, title="Alerta: 10-7-3",
+                                               message="Indique la subclasificación que corresponda")
+            if correct_act == "10-18":
+                options = ["10-18-0", "10-18-1", "10-18-2", "10-18-3", "10-18-4",
+                           "10-18-5", "10-18-6", "10-18-7", "10-18-8"]
+                correct_act = gui.show_options(options, title="Alerta: 10-7-3",
+                                               message="Indique la subclasificación que corresponda")
+            # TODO - Update DF
+            ...
+
+        # Detect DE, ask if it's a DE or EJ-G
+        elif act[19:] == "DE":
+            options = ["DE", "EJ-G"]
+            correct_act = gui.show_options(options, title="Alerta: Delegación",
+                                           message=f"Se detectó una delegación el {day} a las {time}.\n"
+                                                   f"Indique a que acto corresponde en realidad")
+            # TODO - Update DF
+            ...
+    col_names = df.columns.tolist()
+
     dates = ["", "", "Fecha"]
     hours = ["", "", "Hora"]
     acts = ["", "", "Acto"]
@@ -59,11 +166,110 @@ def generate_monthly():
     bonus_df = pd.DataFrame(bonus_dict)
     df = pd.concat([dates_df, hours_df, acts_df, bonus_df, df])
 
-    # TODO - Review 10-6 and 10-7-3
     # TODO - Ask about act length bonus
-    # TODO - Change “F” from volunteers with more than 20 years to “-”
-    # TODO - Change “-” from officers to “F”, as needed
-    # TODO - Calculate statistics
+    ...
+
+    seniority_20 = df[df["Voluntario"] == config.last_with_20]["Ant."].values[0]
+    df["Ant."] = pd.to_numeric(df["Ant."])
+    for series_name, _ in df.iloc[:, 3:].items():
+        # Change “F” from volunteers with more than 20 years to “-”
+        df[series_name] = df[series_name].mask(
+            (df["Ant."] <= seniority_20) & (df[series_name] == "F"), "-", inplace=False
+        )
+
+        # Change “-” from honorary volunteers with less than 20 years to “F”
+        if series_name[19:] in config.mandatory_for_honoraries:
+            df[series_name] = df[series_name].mask(
+                (df["Ant."] > seniority_20)
+                & (df["Cargo"] == "Voluntario Activo")
+                & (df[series_name] == "-"),
+                "F", inplace=False
+            )
+
+        # Change “-” from officers to “F” (captain, lieutenants, assistants)
+        if (series_name[19:] in config.dept_mandatory_acts
+                or series_name[19:] in config.comp_mandatory_acts):
+            df[series_name] = df[series_name].mask(
+                ((df["Cargo"] == "Capitán") |
+                 (df["Cargo"] == "Teniente Primero") |
+                 (df["Cargo"] == "Teniente Segundo") |
+                 (df["Cargo"] == "Teniente Tercero") |
+                 (df["Cargo"] == "Ayudante")
+                 )
+                & (df[series_name] == "-"),
+                "F", inplace=False
+            )
+
+        # Change “-” from officers to “F” (engineer)
+        if ((series_name[19:] in config.dept_mandatory_acts
+             or series_name[19:] in config.comp_mandatory_acts)
+                and series_name[19:] not in ["FU", "RG"]):
+            df[series_name] = df[series_name].mask(
+                (df["Cargo"] == "Maquinista")
+                & (df[series_name] == "-"),
+                "F", inplace=False
+            )
+
+        # Change “-” from officers to “F” (secretary, treasurer, intendent)
+        if ((series_name[19:] in config.dept_mandatory_acts
+             or series_name[19:] in config.comp_mandatory_acts)
+                and series_name[19:] not in ["FU", "RG", "EJ-GEN", "EJ", "10-34", "10-30"]):
+            df[series_name] = df[series_name].mask(
+                ((df["Cargo"] == "Secretario") |
+                 (df["Cargo"] == "Tesorero") |
+                 (df["Cargo"] == "Intendente")
+                 )
+                & (df[series_name] == "-"),
+                "F", inplace=False
+            )
+
+    # Calculate statistics
+    dept_mandatory = ["", "", "", "Ob. Gen."]
+    comp_mandatory = ["", "", "", "Ob. Cía."]
+    others = ["", "", "", "Ab."]
+    total = ["", "", "", "Total"]
+
+    abh = df.iloc[3, 3:].tolist()
+    for n, item in enumerate(abh):
+        if item == "":
+            abh[n] = 1
+        else:
+            abh[n] = int(abh[n])
+
+    sum_dept_mandatory = 0
+    sum_comp_mandatory = 0
+    sum_others = 0
+
+    for _, row in df.iterrows():
+        if row["Ant."] >= 1:
+            series_count = 0
+            for series_name, series in row.items():
+                if series_name == "Ant." or series_name == "Voluntario" or series_name == "Cargo":
+                    continue
+                if series == "A":
+                    if series_name[19:] in config.dept_mandatory_acts:
+                        sum_dept_mandatory += 1
+                        sum_others += abh[series_count] - 1
+                    elif series_name[19:] in config.comp_mandatory_acts:
+                        sum_comp_mandatory += 1
+                        sum_others += abh[series_count] - 1
+                    else:
+                        sum_others += abh[series_count]
+                series_count += 1
+            dept_mandatory.append(sum_dept_mandatory)
+            comp_mandatory.append(sum_comp_mandatory)
+            others.append(sum_others)
+            total.append(sum_dept_mandatory + sum_comp_mandatory + sum_others)
+            sum_dept_mandatory = 0
+            sum_comp_mandatory = 0
+            sum_others = 0
+
+    df["Ob. Gen."] = dept_mandatory
+    df["Ob. Cía."] = comp_mandatory
+    df["Ab."] = others
+    df["Total"] = total
+
+    df = df.copy()
 
     # Export excel: full monthly report
     name = f"{dates[3].year}-{dates[3].month:02d}"
@@ -161,6 +367,36 @@ def generate_monthly():
                 col = f"{col[0]}{chr(ord(col[-1:]) + 1)}"
         to_check = ws[f"{col}1"]
 
+    # Format statistics
+    for i in range(4):
+        ws.merge_cells(f"{col}1:{col}4")
+        to_check = ws[f"{col}1"]
+        to_check.fill = openpyxl.styles.PatternFill("solid", fgColor="004000")
+        to_check.font = openpyxl.styles.Font(bold=True, color="FFFFFF")
+        to_check.alignment = openpyxl.styles.Alignment(vertical="center", horizontal="center",
+                                                       wrap_text=True)
+        ws.column_dimensions[col].width = 7
+        match i:
+            case 0:
+                ws[f"{col}1"] = "Ob. Gen."
+            case 1:
+                ws[f"{col}1"] = "Ob. Cía."
+            case 2:
+                ws[f"{col}1"] = "Ab."
+            case 3:
+                ws[f"{col}1"] = "Total"
+
+        if col[-1:] == "Z":
+            if len(col) == 1:
+                col = "AA"
+            else:
+                col = f"{chr(ord(col[0]) + 1)}A"
+        else:
+            if len(col) == 1:
+                col = f"{chr(ord(col[0]) + 1)}"
+            else:
+                col = f"{col[0]}{chr(ord(col[-1:]) + 1)}"
+
     # Format names and attendance
     ws.column_dimensions["A"].width = 5
     ws.column_dimensions["B"].width = 32
@@ -213,7 +449,14 @@ def generate_monthly():
     wb.save(name)
 
     # TODO - Export excel: simple report
+    ...
     # TODO - Format and color code simple report
+    ...
+
+    # Remove original file after finishing
+    os.remove(file)
+
+    return 0
 
 
 def generate_quarterly():
@@ -229,21 +472,5 @@ def settings():
 
 
 if __name__ == "__main__":
-    '''
-    options = ["Reportes mensuales (cuadro y parte)",
-               "Reportes trimestrales (obligatorios y porcentajes)",
-               "Configuración"]
-    while True:
-        match gui.menu(options):
-            case 1:
-                generate_monthly()
-                break
-            case 2:
-                generate_quarterly()
-                break
-            case 3:
-                settings()
-            case _:
-                print("Not an option")
-    '''
-    generate_monthly()
+    locale.setlocale(locale.LC_ALL, 'es_CL.utf8')
+    sys.exit(main())
