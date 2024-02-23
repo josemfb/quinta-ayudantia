7eaf56bfb1e6443d6c8eb8608baf4aef7f3a4847
José <josemfb@gmail.com>
2024-02-23 14:31:03 -0300

    Quarterly reports moved to new file

---
+++ quarterly.py
@@ -0,0 +1,15 @@
+def generate() -> int:
+    to_return = 0
+    to_return += mandatory()
+    to_return += statistics()
+    return to_return
+
+
+def mandatory() -> int:
+    # TODO - Generate report with mandatory acts of the quarter
+    ...
+
+
+def statistics() -> int:
+    # TODO - Generate report with assistance statistics
+    ...
