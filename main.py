1bcb5ed66375b01a5934797bfdd10d1555c5088d
José <josemfb@gmail.com>
2024-02-27 11:20:15 -0300

    Added menu to configuration

--- main.py
+++ main.py
@@ -13,26 +13,42 @@ def main() -> int:
                "Cuadro de obligatorios trimestrales y porcentajes",
                "Configuración"]
     while True:
-        match gui.show_menu(options):
+        match gui.show_menu(options, title="Automatizador de la Ayudantía"):
             case 0:
                 return monthly.generate()
             case 1:
                 return quarterly.generate()
             case 2:
-                settings()
+                return settings()
             case _:
                 return 1
 
 
 def settings():
-    # TODO - Review and create new types of acts
-    ...
+    options = ["Revisar listado de actos obligatorios",
+               "Modificar último voluntario con premio de 20",
+               "Acerca de este programa", ]
+                # "Crear nuevo tipo de acto"]
 
-    # TODO - Select which acts are mandatory
-    ...
-
-    # TODO - Select last volunteer with 20 years of service
-    ...
+    while True:
+        match gui.show_menu(options, title="Configuración"):
+            case 0:
+                # TODO - Select which acts are mandatory
+                ...
+            case 1:
+                # TODO - Select last volunteer with 20 years of service
+                ...
+            # case 2:
+                # TODO - Review and create new types of acts
+                ...
+            case 2:  # case 3:
+                msg = ("Automatizador de tareas de la ayudantía.\n"
+                       "Creado por José Fernández B.\n"
+                       "jose.fernandez@5.cbs.cl")
+                gui.show_message(title="Acerca de", message=msg)
+                return 0
+            case _:
+                return 1
 
 
 if __name__ == "__main__":
